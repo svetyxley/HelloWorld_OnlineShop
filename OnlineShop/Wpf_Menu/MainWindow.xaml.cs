0b6c5c0b60c7dabb0f8d419e3e31192dbb3f0eb3
NikitaGostev <nik121283@gmail.com>
2024-05-26 23:29:56 +0300

    Добавлены кнопки главного меню

--- OnlineShop/Wpf_Menu/MainWindow.xaml.cs
+++ OnlineShop/Wpf_Menu/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace Wpf_Menu
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
