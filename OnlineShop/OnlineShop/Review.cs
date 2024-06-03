0bf144c11e5b4f68cbc3b7e02bf8545d0528e993
NikitaGostev <nik121283@gmail.com>
2024-06-03 23:39:02 +0300

    Добавил свои классы

---
+++ OnlineShop/OnlineShop/Review.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop
+{
+    internal class Review
+    {
+        public int PaymentType_Id;
+        public paymnetTypes paymnetType;
+
+        public Review()
+        {
+                
+        }
+
+
+    }
+
+    enum paymnetTypes
+    {
+        cash_on_delivery,
+        on_bank_account,
+        by_card_transfer
+    }
+}
