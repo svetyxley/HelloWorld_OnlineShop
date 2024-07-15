f457ea1a44dd86a5e652b437df88c781a109bba4
Svitlana Leibak <svetlanal@devart.com>
2024-07-15 23:03:35 +0300

    Added SvitlanaL configuration

--- OnlineShop/ConsoleApp1/Program.cs
+++ OnlineShop/ConsoleApp1/Program.cs
@@ -1,16 +1,6 @@
-﻿using System.Configuration;
-using System.Reflection;
-using Dapper;
-using FluentMigrator.Runner;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
 using OnlineShop.BusinessLayer.Managers;
 using OnlineShop.BusinessLayer.Services;
-using OnlineShop.Data;
-using OnlineShop.Entities;
-using static System.Formats.Asn1.AsnWriter;
 
 namespace ConsoleApp1
 {
@@ -21,82 +11,17 @@ namespace ConsoleApp1
 
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
-                .AddJsonFile("appsettings.Dev.json")
-                .AddJsonFile("appsettings.Qa.json")
+                .AddJsonFile("appsettings.SvitlanaL.json")
                 .Build();
 
-            var maxUsers = configuration["EdPlatformMaxUsers"];
-            var connectionString = configuration.GetConnectionString("Master");
-
-            var name = configuration["Name"];
-            var timeout = configuration["Timeout"];
-
+            var connectionString = configuration.GetConnectionString("SvitlanaL");
 
             var manager = new ManufacturesService();
             var manufacturer = manager.GetManufacturerByID(1, connectionString);
-
-
-
-
-
 
             ProductsCatalogFlow mainFlow = new ProductsCatalogFlow();
             mainFlow.CatalogProcesses();
         }
     }
-
-
-
-    /*
-     var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddJsonFile("appsettings.Dev.json")
-                .AddJsonFile("appsettings.Release.json")
-                .AddJsonFile("appsettings.QA.json")
-                .Build();
-
-
-            var a = configuration["Name"];
-            var connectionStr = configuration.GetConnectionString("Master");
-
-            using (var connection = new SqlConnection(connectionStr))
-            {
-
-                connection.Open();
-
-    var res = connection.Execute("UpdateМanufacturer", new { @ID = 1, @Name = "Norven1" });
-
-;               var str = "'Norven'; drop table aaa;";
-                var sql = $"select* FROM Manufacturer where [ManufacturerName] like {str}";
-
-                var result = connection
-                    .Query<Manufacturer>(sql);
-
-                connection.Close();
-            }
-      
-
-    var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            var host = Host.CreateDefaultBuilder()
-                .ConfigureServices((context, services) => {
-                    services.AddLogging(c => c.AddFluentMigratorConsole())
-                        .AddFluentMigratorCore()
-                        .ConfigureRunner(c => c.AddSqlServer2012()
-                            .WithGlobalConnectionString(configuration.GetConnectionString("SqlConnection"))
-                            .ScanIn(Assembly.GetExecutingAssembly()).For.Migrations());
-                })
-                //.UseSerilog()
-                .Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var migrationService = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
-                migrationService.ListMigrations();
-                migrationService.MigrateUp();
-            }
-     */
 }
 
